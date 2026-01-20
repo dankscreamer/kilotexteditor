48089b33e94553ec50efc4e78e52a58f797cf803
Adit Singh <aditatpeace@gmail.com>
2026-01-20 09:28:03 +0530

    Finally started showing output in the terminal code prints every character typed in a new line with ascii value

--- kilo.c
+++ kilo.c
@@ -1,24 +1,35 @@
+#include <stdio.h>
+#include <ctype.h>
+
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
+struct termios orig_termios;
 
-void enableRawMode(void) {
-    struct termios raw;
 
-    tcgetattr(STDIN_FILENO, &raw);
-    raw.c_lflag &= ~(ECHO | ICANON);
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+
+
+struct termios orig_termios;
+void disableRawMode(void) {
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
-
+void enableRawMode(void) {
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode);
+  struct termios raw = orig_termios;
+  raw.c_lflag &= ~(ECHO|ICANON);
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 int main(void) {
-    enableRawMode();
-
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1) {
-        if (c == 'q') break;
-        write(STDOUT_FILENO, &c, 1);
+  enableRawMode();
+  char c;
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    if (iscntrl(c)) {
+      printf("%d\n", c);
+    } else {
+      printf("%d ('%c')\n", c, c);
     }
-
-    return 0;
+  }
+  return 0;
 }
 
