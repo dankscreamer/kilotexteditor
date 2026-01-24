ae88ecf9163e2bb2afe5e7fffb7ed8b6cf12eddd
Adit Singh <aditatpeace@gmail.com>
2026-01-24 17:16:12 +0530

    wrote the code from scratch

--- kilo.c
+++ kilo.c
@@ -1,38 +1,67 @@
+#include <unistd.h>
 #include <stdio.h>
+#include <termios.h>
+#include <stdlib.h>
 #include <ctype.h>
 
-#include <stdlib.h>
-#include <termios.h>
-#include <unistd.h>
+
+
 struct termios orig_termios;
 
+void disableRawMode(void){
+    tcsetattr(STDIN_FILENO,TCSAFLUSH,&orig_termios);
 
 
 
 
-void disableRawMode(void) {
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
-}
-void enableRawMode(void) {
-  tcgetattr(STDIN_FILENO, &orig_termios);
-  atexit(disableRawMode);
-  struct termios raw = orig_termios;
-  raw.c_iflag &= ~(IXON | IEXTEN);
-  raw.c_lflag &= ~(ECHO|ICANON | ISIG | IXON | IEXTEN);
-  raw.c_oflag &= ~(OPOST);
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
-}
-int main(void) {
-  enableRawMode();
-  char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'e') {
-    if (iscntrl(c)) {
-      printf("%d\r\n", c);
-    } else {
-      printf("%d ('%c')\r\n", c, c);
+
     }
-  }
-  return 0;
-}
 
 
+
+void enableRawmode(void){
+    
+    tcgetattr(STDIN_FILENO,&orig_termios);
+    atexit(disableRawMode);
+    struct termios raw=orig_termios;
+
+
+    raw.c_iflag &= ~(IXON | ICRNL);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN  );
+
+    tcsetattr(STDIN_FILENO,TCSAFLUSH,&raw);
+                
+
+    }
+
+int main(void){
+    enableRawmode();
+
+
+
+
+    char c; //we create a variable c
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
+        if(iscntrl(c)){
+            printf("%d\n",c);
+        
+
+
+            }else{
+                printf("%d('%c'\n)",c,c);
+
+
+
+                }
+
+        
+        
+
+                    
+        };
+        
+        return 0;
+
+
+    }
